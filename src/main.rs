3430163cb20efd8f0e96a6b4108546361897ab66
Matthew Palmer <matt@silvercreekit.com>
2019-07-25 23:55:00 -0400

    start parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#![warn(rust_2018_idioms)]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+
+use clap::{App, Arg, SubCommand};
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+	let app = App::new("yapb")
+		.version("0.1.0")
+		.about("YAPB CLI utility")
+		.author("Matthew Palmer <mspalmer91@gmail.com>")
+		.subcommand(
+			SubCommand::with_name("push")
+				.about("Pushes files into a new paste")
+				.arg(Arg::with_name("files").multiple(true)),
+		)
+		.subcommand(SubCommand::with_name("pull").about("Pulls files when given URL"))
+		.get_matches();
+
+	match app.subcommand() {
+		("push", Some(_cmd_name)) => {
+			println!("Push!");
+		}
+		("pull", Some(_cmd_name)) => {
+			println!("Pull!");
+		}
+		_ => {
+			eprintln!("{}", app.usage());
+			process::exit(1);
+		}
+	}
 }
