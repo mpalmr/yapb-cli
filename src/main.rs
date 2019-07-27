5681bde305d2b4c67751af53c8a33775592c9c45
Matthew Palmer <matt@silvercreekit.com>
2019-07-27 11:43:08 -0400

    read files

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,46 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg, SubCommand, Values};
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
 use std::process;
 
-fn read_files(files: &[&str]) -> Result<(), String> {
+#[derive(Debug)]
+struct PasteFile {
+	name: String,
+	contents: String,
+}
+
+impl PasteFile {
+	pub fn new(name: &str) -> Result<Self, String> {
+		let file = match File::open(name) {
+			Ok(file) => file,
+			Err(e) => match e.kind() {
+				io::ErrorKind::NotFound => return Err(format!("File not found: {}", name)),
+				io::ErrorKind::PermissionDenied => {
+					return Err(format!("Permission denied: {}", name))
+				}
+				_ => return Err(format!("Unknown error: {}", name)),
+			},
+		};
+
+		let mut buf_reader = BufReader::new(file);
+		let mut contents = String::new();
+		buf_reader
+			.read_to_string(&mut contents)
+			.map_err(|e| e.to_string())?;
+
+		Ok(Self {
+			name: name.to_string(),
+			contents,
+		})
+	}
+}
+
+fn create_paste(files: &Vec<PasteFile>) {
 	println!("{:?}", files);
-	Ok(())
 }
 
 fn main() {
@@ -29,11 +63,17 @@ fn main() {
 
 	match app.subcommand() {
 		("new", Some(matches)) => {
-			let file_names: Vec<&str> = matches.values_of("files").unwrap().collect();
-			if let Err(err) = read_files(&file_names) {
-				eprintln!("Error: {}", err);
-				process::exit(1);
-			}
+			let file_names: Values<'_> = matches.values_of("files").unwrap();
+			let files = file_names
+				.map(|name| match PasteFile::new(name) {
+					Ok(file) => file,
+					Err(e) => {
+						eprintln!("{}", e);
+						process::exit(1);
+					}
+				})
+				.collect::<Vec<PasteFile>>();
+			create_paste(&files);
 		}
 		("get", Some(matches)) => {
 			println!("{:?}", matches);
