b8bc7de797f59e62c295070854c484805d6a6221
Matthew Palmer <matt@silvercreekit.com>
2019-07-27 10:22:21 -0400

    get files from args

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,38 @@
 use clap::{App, Arg, SubCommand};
 use std::process;
 
+fn read_files(files: &[&str]) -> Result<(), String> {
+	println!("{:?}", files);
+	Ok(())
+}
+
 fn main() {
 	let app = App::new("yapb")
 		.version("0.1.0")
 		.about("YAPB CLI utility")
 		.author("Matthew Palmer <mspalmer91@gmail.com>")
 		.subcommand(
-			SubCommand::with_name("push")
-				.about("Pushes files into a new paste")
-				.arg(Arg::with_name("files").multiple(true)),
+			SubCommand::with_name("new")
+				.about("creates a new paste and returns the URL")
+				.arg(Arg::with_name("files").required(true).multiple(true)),
 		)
-		.subcommand(SubCommand::with_name("pull").about("Pulls files when given URL"))
+		.subcommand(
+			SubCommand::with_name("get")
+				.about("retreives a paste")
+				.arg(Arg::with_name("url").required(true)),
+		)
 		.get_matches();
 
 	match app.subcommand() {
-		("push", Some(_cmd_name)) => {
-			println!("Push!");
+		("new", Some(matches)) => {
+			let file_names: Vec<&str> = matches.values_of("files").unwrap().collect();
+			if let Err(err) = read_files(&file_names) {
+				eprintln!("Error: {}", err);
+				process::exit(1);
+			}
 		}
-		("pull", Some(_cmd_name)) => {
-			println!("Pull!");
+		("get", Some(matches)) => {
+			println!("{:?}", matches);
 		}
 		_ => {
 			eprintln!("{}", app.usage());
