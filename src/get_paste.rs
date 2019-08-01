fd9c98dc8589b4f1192001cafa710e926e912239
Matthew Palmer <matt@silvercreekit.com>
2019-08-01 14:10:45 -0400

    iterativly write files

--- src/get_paste.rs
+++ src/get_paste.rs
@@ -2,6 +2,9 @@ use crate::HTTP_ORIGIN;
 use reqwest::Client;
 use serde::Deserialize;
 use std::error::Error;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 use std::path::Path;
 
 #[derive(Deserialize, Debug)]
@@ -20,9 +23,17 @@ struct Paste {
 pub fn fetch(id: &str, target: &str) -> Result<(), Box<dyn Error>> {
 	let url = format!("{}/api/paste/{}", HTTP_ORIGIN, id);
 	let paste: Paste = Client::new().get(&url).send()?.json()?;
-	let path = Path::new(target);
-	paste.files.iter().for_each(|file| {
-		println!("{:?}", file);
-	});
-	Ok(())
+	let target_path = Path::new(target);
+	match paste
+		.files
+		.iter()
+		.map(|file| {
+			File::create(target_path.join(&file.name))
+				.and_then(|mut f| f.write_all(file.contents.as_bytes()))
+		})
+		.collect::<Result<(), io::Error>>()
+	{
+		Ok(_) => Ok(()),
+		Err(e) => Err(Box::from(e)),
+	}
 }
